d4e7931e70709aa8d0c61e7a690a08fc9a652a3b
Mercy <olorungbogo04@gmail>
2025-09-24 10:16:02 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import Home  from './pages/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Service from './pages/service/service';
-import Experience from './pages/experience/experience';
-import About from './pages/about/about';
+import Service from './pages/service/Service';
+import Experience from './pages/experience/Experience';
+import About from './pages/about/About';
 import Faqs from './pages/FAQs/Faqs';
 
 
